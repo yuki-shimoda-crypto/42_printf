a4e4f70df4c25f4e74c5a115d20aa893be71601c
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-01 12:42:21 +0900

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -13,6 +13,33 @@
 #include "libft/libft.h"
 #include "ft_printf.h"
 
+static size_t ft_put_int(int d)
+{
+	char	*num;
+	size_t	printed;
+
+	num = ft_itoa(d);
+	printed = ft_strlen(num);
+	write(1, num, printed);
+	ft_itoa(d);
+	return (printed);
+}
+
+static size_t ft_put_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+static size_t ft_put_str(char *str)
+{
+	size_t	printed;
+
+	printed = ft_strlen(str);
+	write(1, str, printed);
+	return (printed);
+}
+
 static void ft_init_flags(t_flags *specication)
 {
 	// specication->flags = -1;
@@ -23,45 +50,63 @@ static void ft_init_flags(t_flags *specication)
 	// specication->putnum = -1;
 }
 
-static void	ft_proc_per(const char **start, const char **format, int *n)
+static void	ft_proc_per(const char **start, const char **format, size_t *printed, va_list ap)
 {
-	t_flags	specication;
+	// t_flags	specication;
 	
-	specication.conversion = ft_strchr("cspdiuxX%", (int)**start);
+	// specication.conversion = ft_strchr("cspdiuxX%", **start);
+	(*format)++;
+	if (**format == 'c')
+		*printed += ft_put_char(va_arg(ap, int));
+	else if (**format == 's')
+		*printed += ft_put_str(va_arg(ap, char *));
+	else if (**format == 'p')
+		*printed += ft_put_ptr(va_arg(ap, char *));
+	else if (**format == 'd')
+		*printed += ft_put_int(va_arg(ap, int));
+	else if (**format == 'i')
+		*printed += ft_put_int(va_arg(ap, int));
+	else if (**format == 'u')
+		*printed += ft_put_udec(va_arg(ap, int));
+	else if (**format == 'x')
+		*printed += ft_put_uhex(va_arg(ap, int));
+	else if (**format == 'X')
+		*printed += ft_put_uhex(va_arg(ap, int));
+	(*format)++;
 }
 
-static void	ft_print_str(const char **start, const char **format, int *n)
+static void	ft_print_str(const char **start, const char **format, size_t *printed)
 {
 	while (**format != '%' && **format)
 		(*format)++;
 	write(1, *start, *format - *start);
-	*n += *format - *start;
+	*printed += *format - *start;
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list		ap;
-	int			n;
+	size_t		printed;
 	const char *start;
 
-	n = 0;
+	printed = 0;
 	va_start(ap, format);
-	while (*format)
+	while (*format && printed <= INT_MAX)
 	{
 		start = format;
 		if (*format != '%')
-			ft_print_str(&start, &format, &n);
-		// else
-		// 	ft_proc_per(&start, &format, &n);
+			ft_print_str(&start, &format, &printed);
+		else
+			ft_proc_per(&start, &format, &printed, ap);
 	}
 	va_end(ap);
-	return (n);
+	return (printed);
 }
 
 int main(void)
 {
 	// printf("%zu\n", ft_strlen("strlen"));
-	ft_printf("aiueol");
+	ft_printf("abc\n%c\n%s\n", 'd', "def");
 	return (0);
 }
 
--- test.c
+++ test.c
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+
+int ft_printf(const char *format, ...)
+{
+
+}
 
 int main(void)
 {
