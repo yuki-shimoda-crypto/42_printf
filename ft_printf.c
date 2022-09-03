c5c4e350083c3343343aba6d65d9b2b8f1f64f58
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-03 15:28:54 +0900

    change header

--- ft_printf.c
+++ ft_printf.c
@@ -5,13 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/27 15:46:49 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/08/27 19:16:30y yshimoda         ###   ########.fr       */
+/*   Created: 2022/09/03 15:20:46 by yshimoda          #+#    #+#             */
+/*   Updated: 2022/09/03 15:21:36 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "ft_printf.h"
+#include "libft/libft.h"
 
 static void	ft_proc_per(const char **format, size_t *printed, va_list ap)
 {
@@ -22,25 +22,19 @@ static void	ft_proc_per(const char **format, size_t *printed, va_list ap)
 		*printed += ft_put_str(va_arg(ap, char *));
 	else if (**format == 'p')
 		*printed += ft_put_hex(format, (uintptr_t)va_arg(ap, uintptr_t));
-	else if (**format == 'd')
-		*printed += ft_put_int(format, (int)va_arg(ap, int));
-	else if (**format == 'i')
+	else if (**format == 'd' || **format == 'i')
 		*printed += ft_put_int(format, (int)va_arg(ap, int));
 	else if (**format == 'u')
 		*printed += ft_put_int(format, (unsigned int)va_arg(ap, unsigned int));
-	else if (**format == 'x')
-		*printed += ft_put_hex(format, va_arg(ap, unsigned int));
-	else if (**format == 'X')
+	else if (**format == 'x' || **format == 'X')
 		*printed += ft_put_hex(format, va_arg(ap, unsigned int));
 	else if (**format == '%')
-	{
-		*printed += 1;
-		write(1, "%", 1);
-	}
+		*printed += write(1, "%", 1);
 	(*format)++;
 }
 
-static void	ft_print_str(const char **start, const char **format, size_t *printed)
+static void	ft_print_str(const char **start, const char **format,
+		size_t *printed)
 {
 	while (**format != '%' && **format)
 		(*format)++;
