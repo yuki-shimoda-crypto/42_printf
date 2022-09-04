c5c67a44f2c777ff1b5cb30b00ff1b9403f630e5
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-04 23:44:18 +0900

    fix ft_put_ptr

--- ft_put_ptr.c
+++ ft_put_ptr.c
@@ -22,8 +22,8 @@ static void	ft_output(uintptr_t num, ssize_t *num_len)
 {
 	if (num >= 16)
 	{
-		ft_put_ptr(num / 16, num_len);
-		ft_put_ptr(num % 16, num_len);
+		ft_output(num / 16, num_len);
+		ft_output(num % 16, num_len);
 	}
 	else
 	{
@@ -42,6 +42,6 @@ ssize_t	ft_put_ptr(uintptr_t num)
 	if (!num)
 		num_len += write(1, "0", 1);
 	else
-		ft_put_ptr(num, &num_len);
+		ft_output(num, &num_len);
 	return (num_len);
 }
