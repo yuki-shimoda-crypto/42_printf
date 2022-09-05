e9dccf6975f9032967f52d80d119ee73ff5d948b
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-05 13:36:43 +0900

    modify printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 15:20:46 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/09/04 23:27:01 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/09/05 13:32:19 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ static void	ft_proc_per(const char **format, ssize_t *printed, va_list *ap)
 		*printed += write(1, "%", 1);
 	(*format)++;
 }
-// va_argをキャストするのはreturn値がvoidポインタが帰ってきてしまうときが
 
 static void	ft_print_str(const char **format, ssize_t *printed)
 {
@@ -91,15 +90,57 @@ int	ft_printf(const char *format, ...)
 	return (printed);
 }
 
-// int main(void)
+// #include "ft_printf.h"
+// #include "libft/libft.h"
+// #include <stdio.h>
+
+// int	main(void)
 // {
 // 	int	i;
 
 // 	i = 0;
-	
-// 	ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\n", 'd', "def", 1, 10, 100,
-// 			1000, 1000, &i);
-// 	printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\n", 'd', "def", 1, 10, 100,
-// 			1000, 1000, &i);
+// 	printf("#c\n");
+// 	ft_printf("%c\t%c\n", 'c', '%');
+// 	printf("%c\t%c\n\n", 'c', '%');
+
+// 	printf("#str\n");
+// 	ft_printf("%s\t%s\t%s\n", "42Tokyo", "", NULL);
+// 	printf("%s\t%s\t%s\n\n", "42Tokyo", "", NULL);
+
+// 	printf("#int\n");
+// 	ft_printf("%d\t%d\t%d\t%d\n", i, 100, INT_MAX, INT_MIN);
+// 	printf("%d\t%d\t%d\t%d\n\n", i, 100, INT_MAX, INT_MIN);
+
+// 	printf("#uint\n");
+// 	ft_printf("%u\t%u\t%u\n", i, 100, UINT_MAX);
+// 	printf("%u\t%u\t%u\n\n", i, 100, UINT_MAX);
+
+// 	printf("#hex\n");
+// 	printf("%x\t%x\t%x\n", i, 255, UINT_MAX);
+// 	printf("%x\t%x\t%x\n\n", i, 255, UINT_MAX);
+
+// 	printf("#HEX\n");
+// 	printf("%x\t%X\t%X\n", i, 255, UINT_MAX);
+// 	printf("%x\t%X\t%X\n\n", i, 255, UINT_MAX);
+
+// 	printf("#ptr\n");
+// 	ft_printf("%p\t%p\t%p\t%p\n", &i, SIZE_MAX, NULL, 0);
+// 	printf("%p\t%p\t%p\t%p\n\n", &i, SIZE_MAX, NULL, 0);
+
+// 	printf("#percent\n");
+// 	ft_printf("%%\n");
+// 	printf("%%\n\n");
+
+// 	printf("#mix\n");
+// 	ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'd', "def", 1, 10,
+// 			100, 100, 100, &i);
+// 	printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n\n", 'd', "def", 1, 10, 100,
+// 			100, 100, &i);
+
+// 	printf("#return\n");
+// 	printf("%d\n", ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'c',
+// 				"str", 1, 10, 100, 255, 255, &i));
+// 	printf("%d\n", printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'c',
+// 				"str", 1, 10, 100, 255, 255, &i));
 // 	return (0);
 // }
--- main.c
+++ main.c
@@ -6,43 +6,61 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 15:46:49 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/09/05 12:36:51 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/09/05 12:36:51y yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "ft_printf.h"
+#include "libft/libft.h"
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
 	int	i;
 
 	i = 0;
-	
-	ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\n", 'd', "def", 1, 10, 100,
-			1000, 1000, &i);
-	printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\n", 'd', "def", 1, 10, 100,
-			1000, 1000, &i);
+	printf("#c\n");
+	ft_printf("%c\t%c\n", 'c', '%');
+	printf("%c\t%c\n\n", 'c', '%');
+
+	printf("#str\n");
+	ft_printf("%s\t%s\t%s\n", "42Tokyo", "", NULL);
+	printf("%s\t%s\t%s\n\n", "42Tokyo", "", NULL);
+
 	printf("#int\n");
 	ft_printf("%d\t%d\t%d\t%d\n", i, 100, INT_MAX, INT_MIN);
-	printf("%d\t%d\t%d\t%d\n", i, 100, INT_MAX, INT_MIN);
+	printf("%d\t%d\t%d\t%d\n\n", i, 100, INT_MAX, INT_MIN);
 
 	printf("#uint\n");
 	ft_printf("%u\t%u\t%u\n", i, 100, UINT_MAX);
-	printf("%u\t%u\t%u\n", i, 100, UINT_MAX);
+	printf("%u\t%u\t%u\n\n", i, 100, UINT_MAX);
 
 	printf("#hex\n");
 	printf("%x\t%x\t%x\n", i, 255, UINT_MAX);
-	printf("%x\t%x\t%x\n", i, 255, UINT_MAX);
+	printf("%x\t%x\t%x\n\n", i, 255, UINT_MAX);
 
 	printf("#HEX\n");
 	printf("%x\t%X\t%X\n", i, 255, UINT_MAX);
-	printf("%x\t%X\t%X\n", i, 255, UINT_MAX);
+	printf("%x\t%X\t%X\n\n", i, 255, UINT_MAX);
 
 	printf("#ptr\n");
 	ft_printf("%p\t%p\t%p\t%p\n", &i, SIZE_MAX, NULL, 0);
-	printf("%p\t%p\t%p\t%p\n", &i, SIZE_MAX, NULL, 0);
-	return (0);
+	printf("%p\t%p\t%p\t%p\n\n", &i, SIZE_MAX, NULL, 0);
 
-} 
+	printf("#percent\n");
+	ft_printf("%%\n");
+	printf("%%\n\n");
+
+	printf("#mix\n");
+	ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'd', "def", 1, 10,
+			100, 100, 100, &i);
+	printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n\n", 'd', "def", 1, 10, 100,
+			100, 100, &i);
+
+	printf("#return\n");
+	printf("%d\n", ft_printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'c',
+				"str", 1, 10, 100, 255, 255, &i));
+	printf("%d\n", printf("abc\t%c\t%s\t%d\t%i\t%u\t%x\t%X\t%p\t%%\n", 'c',
+				"str", 1, 10, 100, 255, 255, &i));
+	return (0);
+}
